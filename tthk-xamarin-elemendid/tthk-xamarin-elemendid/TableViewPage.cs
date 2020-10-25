c7d7f98f0f066580fa4ede61b8bdef3b5dc744ed
Nikolas Laus <bchik3@gmail.com>
2020-10-25 19:43:05 +0200

    Add WebView with buttons.

--- tthk-xamarin-elemendid/tthk-xamarin-elemendid/TableViewPage.cs
+++ tthk-xamarin-elemendid/tthk-xamarin-elemendid/TableViewPage.cs
@@ -8,15 +8,61 @@ using Xamarin.Forms;
 namespace tthk_xamarin_elemendid
 {
 	public class TableViewPage : ContentPage
-{
-    public TableViewPage()
     {
-        Content = new StackLayout
+        public TableViewPage()
         {
-            Children = {
-                    new Label { Text = "Welcome to Xamarin.Forms!" }
+            TableView tableView = new TableView()
+            {
+                Root = new TableRoot()
+                {
+                    new TableSection("Esimene")
+                    {
+                        new SwitchCell() { Text = "Esimene" },
+                        new SwitchCell() { Text = "Teine" }
+                    },
+                    new TableSection("Teine")
+                    {
+                        new EntryCell() { Label = "Esimene" },
+                        new EntryCell()
+                        {
+                            Label = "Teine",
+                            LabelColor = Color.Chocolate
+                        }
+                    },
+                    new TableSection("Kolmas")
+                    {
+                        new ViewCell()
+                        {
+                            View = new StackLayout()
+                            {
+                                Children = { new Slider(0, 100, 10)
+                                    {
+                                        MinimumTrackColor = Color.Red,
+                                        MaximumTrackColor = Color.Green,
+                                        VerticalOptions = LayoutOptions.CenterAndExpand
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
-        };
+            };
+            foreach (var i in tableView.Root[0])
+            {
+                if (i is SwitchCell switchCell) switchCell.OnChanged += AnySwitchCellChanged;
+            }
+
+            Content = tableView;
+        }
+
+        private async void AnySwitchCellChanged (object sender, ToggledEventArgs e)
+        {
+            if (sender is SwitchCell switchCell)
+            {
+                string switchCellState = switchCell.On ? "sisse" : "välja";
+                await DisplayAlert("SwitchCell muudetud", $"{switchCell.Text} switchCell " +
+                                                          $"muudetud {switchCellState}", "OK");
+            }
+        }
     }
-}
 }
--- tthk-xamarin-elemendid/tthk-xamarin-elemendid/WebViewPage.cs
+++ tthk-xamarin-elemendid/tthk-xamarin-elemendid/WebViewPage.cs
@@ -1,22 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+using System.Net;
 using Xamarin.Forms;
 
 namespace tthk_xamarin_elemendid
 {
 	public class WebViewPage : ContentPage
-{
-    public WebViewPage()
     {
-        Content = new StackLayout
+        private readonly WebView _webView;
+        private const string TimetablePage = "https://thk.edupage.org/timetable/";
+        private const string HomePage = "https://www.tthk.ee/";
+        private const string ChangesPage = "https://tthk.ee/tunniplaani-muudatused";
+        private const string ConsultationsPage = "https://www.tthk.ee/oppetoo/opetajate-konsultatsioonid/";
+        private ImageButton _previousButton;
+        private ImageButton _homeButton;
+        private ImageButton _nextButton;
+
+        public WebViewPage()
         {
-            Children = {
-                    new Label { Text = "Welcome to Xamarin.Forms!" }
-                }
-        };
+            ImageButton[] buttons = {_previousButton, _homeButton, _nextButton};
+            string[] buttonsIcons = {"previous.png", "home.png", "next.png"}; // C# updates is a miracle
+            StackLayout buttonsLayout = new StackLayout()
+            {
+                Orientation = StackOrientation.Horizontal
+            };
+            for (int i = 0; i < 3; i++)
+            {
+                buttons[i] = new ImageButton()
+                {
+                    Source = buttonsIcons[i],
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    HeightRequest = 50,
+                    Padding = 10
+                };
+                buttonsLayout.Children.Add(buttons[i]);
+            }
+            buttons[0].Clicked += OpenPreviousPage;
+            buttons[1].Clicked += OpenNextPage;
+            buttons[2].Clicked += OpenHomePage;
+            ToolbarItem linksButton = new ToolbarItem() {Text = "Lehed"};
+            ToolbarItems.Add(linksButton);
+            linksButton.Clicked += LinksButtonOnClicked;
+            _webView = new WebView()
+            {
+                Source = HomePage,
+                WidthRequest = 300,
+                HeightRequest = 1000
+            };
+            _webView.Cookies = new CookieContainer();
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { buttonsLayout, _webView }
+            };
+            Content = stackLayout;
+        }
+
+        private void OpenHomePage(object sender, EventArgs e)
+        {
+            ChangePageOnWebView(HomePage);
+        }
+
+        private void OpenNextPage(object sender, EventArgs e)
+        {
+            _webView.GoForward();
+        }
+
+        private void OpenPreviousPage(object sender, EventArgs e)
+        {
+            _webView.GoBack();
+        }
+
+        private void ChangePageOnWebView(string url)
+        {
+            _webView.Source = url;
+        }
+
+        private async void LinksButtonOnClicked(object sender, EventArgs e)
+        {
+            string selectedPage = await DisplayActionSheet("Lehed", "Avaleht", "Katkesta", "Tunniplaan", "Muudatused", "Konsultatsioonid");
+            switch (selectedPage)
+            {
+                case "Katkesta":
+                    await Navigation.PopAsync();
+                    break;
+                case "Tunniplaan":
+                    ChangePageOnWebView(TimetablePage);
+                    break;
+                case "Muudatused":
+                    ChangePageOnWebView(ChangesPage);
+                    break;
+                case "Konsultatsioonid":
+                    ChangePageOnWebView(ConsultationsPage);
+                    break;
+                default:
+                    ChangePageOnWebView(HomePage);
+                    break;
+            }
+        }
     }
-}
 }
