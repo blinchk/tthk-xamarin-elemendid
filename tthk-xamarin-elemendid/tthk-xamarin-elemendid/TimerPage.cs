b924345d45f8e16091ebfae26522aa58bdccefeb
Nikolas Laus <bchik3@gmail.com>
2020-10-26 08:59:46 +0200

    TimerPage added with button.

--- tthk-xamarin-elemendid/tthk-xamarin-elemendid/TimerPage.cs
+++ tthk-xamarin-elemendid/tthk-xamarin-elemendid/TimerPage.cs
@@ -2,21 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Timers;
 using Xamarin.Forms;
 
 namespace tthk_xamarin_elemendid
 {
 	public class TimerPage : ContentPage
-{
-    public TimerPage()
     {
-        Content = new StackLayout
+        private Button timerButton;
+        public TimerPage()
         {
-            Children = {
-                    new Label { Text = "Welcome to Xamarin.Forms!" }
-                }
-        };
+            Title = "Timer";
+            
+            AbsoluteLayout absoluteLayout = new AbsoluteLayout();
+            timerButton = new Button()
+            {
+                Text = "Vajuta nuppu"
+            };
+            timerButton.Clicked += TimerButtonClicked;
+            AbsoluteLayout.SetLayoutBounds(timerButton, new Rectangle(0.5, 0.5, 110, 100));
+            AbsoluteLayout.SetLayoutFlags(timerButton, AbsoluteLayoutFlags.PositionProportional);
+            absoluteLayout.Children.Add(timerButton);
+            Content = absoluteLayout;
+        }
+
+        private void TimerButtonClicked(object sender, EventArgs e)
+        {
+            TimeSpan timerTime = new TimeSpan(0, 0, 60);
+            Device.StartTimer(timerTime, () =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    // TODO: Timer!!!
+                });
+                return true;
+            });
+        }
     }
-}
 }
