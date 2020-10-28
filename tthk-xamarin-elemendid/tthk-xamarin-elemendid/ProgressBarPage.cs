3190630460b91d85ab445c147f73c46e8d5ebc93
Nikolas Laus <bchik3@gmail.com>
2020-10-29 00:24:38 +0200

    Addd TimerPage with working timer.

---
+++ tthk-xamarin-elemendid/tthk-xamarin-elemendid/ProgressBarPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace tthk-xamarin-elemendid
+{
+	public class ProgressBarPage : ContentPage
+{
+    public ProgressBarPage()
+    {
+        Content = new StackLayout
+        {
+            Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+        };
+    }
+}
+}
--- tthk-xamarin-elemendid/tthk-xamarin-elemendid/TimerPage.cs
+++ tthk-xamarin-elemendid/tthk-xamarin-elemendid/TimerPage.cs
@@ -4,23 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Timers;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace tthk_xamarin_elemendid
 {
 	public class TimerPage : ContentPage
     {
+        private TimeSpan timerTime;
         private Button timerButton;
         public TimerPage()
         {
             Title = "Timer";
-            
             AbsoluteLayout absoluteLayout = new AbsoluteLayout();
             timerButton = new Button()
             {
                 Text = "Vajuta nuppu"
             };
             timerButton.Clicked += TimerButtonClicked;
-            AbsoluteLayout.SetLayoutBounds(timerButton, new Rectangle(0.5, 0.5, 110, 100));
+            AbsoluteLayout.SetLayoutBounds(timerButton, new Rectangle(0.5, 0.5, 110, 100));    
             AbsoluteLayout.SetLayoutFlags(timerButton, AbsoluteLayoutFlags.PositionProportional);
             absoluteLayout.Children.Add(timerButton);
             Content = absoluteLayout;
@@ -28,14 +29,14 @@ namespace tthk_xamarin_elemendid
 
         private void TimerButtonClicked(object sender, EventArgs e)
         {
-            TimeSpan timerTime = new TimeSpan(0, 0, 60);
-            Device.StartTimer(timerTime, () =>
-            {
-                Device.BeginInvokeOnMainThread(() =>
-                {
-                    // TODO: Timer!!!
-                });
-                return true;
+            Device.StartTimer(TimeSpan.FromSeconds(1), () => {
+                        timerTime += TimeSpan.FromSeconds(1);
+                        timerButton.Text = timerTime.ToString(@"mm\:ss");
+                        if (timerTime.TotalSeconds > 60)
+                        {
+                            return false;
+                        }
+                        return true;
             });
         }
     }
