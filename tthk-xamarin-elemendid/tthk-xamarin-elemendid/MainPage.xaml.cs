fcfc798f512539c97aaa67319d7982e6a20929d9
Nikolas Laus <bchik3@gmail.com>
2020-10-29 00:56:47 +0200

    Add ProgressBarPage.cs

--- tthk-xamarin-elemendid/tthk-xamarin-elemendid/MainPage.xaml.cs
+++ tthk-xamarin-elemendid/tthk-xamarin-elemendid/MainPage.xaml.cs
@@ -8,8 +8,8 @@ namespace tthk_xamarin_elemendid
     public partial class MainPage : ContentPage
     {
         List<Button> buttons = new List<Button>();
-        string[] buttonNames = new string[] { "Entry/Editor", "ListView", "DatePicker", "TableView", "WebView", "Timer", "IndicatorView"};
-        Page[] pages = new Page[] { new EntryPage(), new ListViewPage(), new DatePickerPage(), new TableViewPage(), new WebViewPage(), new TimerPage() };
+        string[] buttonNames = new string[] { "Entry/Editor", "ListView", "DatePicker", "TableView", "WebView", "Timer", "ProgressBarPage"};
+        Page[] pages = new Page[] { new EntryPage(), new ListViewPage(), new DatePickerPage(), new TableViewPage(), new WebViewPage(), new TimerPage(), new ProgressBarPage()};
         StackLayout stackLayout = new StackLayout();
         public MainPage()
         {
--- tthk-xamarin-elemendid/tthk-xamarin-elemendid/ProgressBarPage.cs
+++ tthk-xamarin-elemendid/tthk-xamarin-elemendid/ProgressBarPage.cs
@@ -5,18 +5,77 @@ using System.Text;
 
 using Xamarin.Forms;
 
-namespace tthk-xamarin-elemendid
+namespace tthk_xamarin_elemendid
 {
 	public class ProgressBarPage : ContentPage
-{
-    public ProgressBarPage()
     {
-        Content = new StackLayout
+        private readonly ProgressBar _progressBar;
+        private readonly Label sliderPositionLabel;
+        private readonly Slider slider;
+        public ProgressBarPage()
         {
-            Children = {
-                    new Label { Text = "Welcome to Xamarin.Forms!" }
-                }
-        };
+           
+            _progressBar = new ProgressBar()
+            {
+                ProgressColor = Color.DeepPink,
+                Progress = 0.0
+            };
+            Button startButton = new Button()
+            {
+                Text = "Alusta",
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            startButton.Clicked += StartButtonClicked;
+            Button renewButton = new Button()
+            {
+                Text = "Uuendada",
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            renewButton.Clicked += RenewButtonClicked;
+            StackLayout buttonsLayout = new StackLayout()
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children = {startButton, renewButton}
+            };
+            slider = new Slider()
+            {
+                Maximum = 1000,
+                Minimum = 200,
+                MinimumTrackColor = Color.DeepPink,
+                MaximumTrackColor = Color.DeepSkyBlue,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            slider.ValueChanged += SliderValueChanged;
+            sliderPositionLabel = new Label()
+            {
+                Text = slider.Value.ToString()
+            };
+            StackLayout sliderLayout = new StackLayout()
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children = {sliderPositionLabel, slider},
+                Margin = 10
+            };
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { buttonsLayout, sliderLayout, _progressBar }
+            };
+            Content = stackLayout;
+        }
+
+        private void SliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            sliderPositionLabel.Text = Math.Round(slider.Value).ToString();
+        }
+
+        private void RenewButtonClicked(object sender, EventArgs e)
+        {
+            _progressBar.Progress = 0.0;
+        }
+
+        private async void StartButtonClicked(object sender, EventArgs e)
+        {
+            await _progressBar.ProgressTo(1.0, (uint)slider.Value, Easing.BounceOut);
+        }
     }
-}
 }
